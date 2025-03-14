74b8582fc7dd4fcdb991137f8aeae653bdbbcfd8
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-03-14 22:17:42 +0530

    implement AudioBook file

--- AudioBook.java
+++ AudioBook.java
@@ -1,3 +1,13 @@
-public class AudioBook {
-    
+class AudioBook extends Book {
+    private double duration;
+
+    public AudioBook(String title, String author, double duration) {
+        super(title, author);
+        this.duration = duration;
+    }
+
+    @Override
+    public String getInfo() {
+        return super.getInfo() + ", Duration: " + duration + " hours";
+    }
 }
