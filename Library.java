66a35fbfc020a7b6396c7800d152c1d3dc99d2f0
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-03-14 22:04:08 +0530

    implement Library file

--- Library.java
+++ Library.java
@@ -3,3 +3,15 @@ There are diffrent type of books are available in the
 library like Printed books, E books, and Audio books
 
 You must use inhertance, polymorphism, encapuslation...*/
+
+public class Library {
+    public static void main(String[] args) {
+        PrintedBook printedBook = new PrintedBook("The Great Gatsby", "F. Scott Fitzgerald", 218);
+        EBook ebook = new EBook("1984", "George Orwell",  "PDF");
+        AudioBook audiobook = new AudioBook("To Kill a Mockingbird", "Harper Lee",  12.5);
+
+        System.out.println(printedBook.getInfo());
+        System.out.println(ebook.getInfo());
+        System.out.println(audiobook.getInfo());
+    }
+}
